7bdd46c00f8d9d8359aecb828b4dfbc9992f9c6c
Chaker Aloui <aloui-choko@hotmail.fr>
2024-03-28 10:26:39 +0100

    Update MDS_gromacs.sh

--- MDS_gromacs.sh
+++ MDS_gromacs.sh
@@ -5,25 +5,13 @@ tput bold
 echo -e "Chaker ALOUI \n"
 echo -e "Running gromacs MDS ...\n"
 
-configPath="./configs"
+# usage : ./MDS_gromacs.sh AAAA (la proteine sous forme AAAA.pdb doit être dans le repertoire de travail).
+
+#configPath="./configs"
 #dataPath="./data" # mettre les models pdb ici
 #prot2simulate="./data/model.pdb" # a changer a chaque fois
-outPutDir="simulation_date" # à modifier à chaque analyse
-mkdir $outPutDir
-
-#pip install propka
-# Vérifie si 'propka' est installé en utilisant pip list
-if pip list | grep -F propka > /dev/null; then
-    echo "Le package 'propka' est déjà installé."
-else
-    echo "Installation du package 'propka'..."
-    pip install propka
-    if [ $? -eq 0 ]; then
-        echo "'propka' a été installé avec succès."
-    else
-        echo "L'installation de 'propka' a échoué."
-    fi
-fi
+#outPutDir="simulation_date" # à modifier à chaque analyse
+#mkdir $outPutDir
 
 ############TOPOLOGY##########################
 ##1##pdb2gmx to generate topolgy, restrain and processed pdb file used to generate a file that can be used by GROMACS
@@ -34,13 +22,8 @@ fi
 #echo "Downloading $1.pdb "
 #wget http://files.rcsb.org/download/$1.pdb
 
-echo ""
-FoldX --command=RepairPDB --pdb=$1.pdb
-
-
-echo "Assessing the protonation states of the protein.."
-propka3 $1\_Repair.pdb  #_clean.pdb
-
+echo " "
+FoldX --command=RepairPDB --pdb=$1.pdb # ça donne un fichier AAAA_Repair.pdb
 
 # script a partir d un tuto internet:
 # Lysozyme PDB code 1AKI http://www.mdtutorials.com/gmx/lysozyme/01_pdb2gmx.html
@@ -54,32 +37,32 @@ propka3 $1\_Repair.pdb  #_clean.pdb
 #wget http://www.mdtutorials.com/gmx/lysozyme/Files/md.mdp
 
 # Step One: Prepare the Topology
-grep -v HOH $prot2simulate > prot_clean.pdb # enlever l eau
+grep -v HOH $1\_Repair.pdb > $1\_clean.pdb # enlever l eau
 
 #Creation of topology file 
-printf "15 0\n" | gmx pdb2gmx -f prot_clean.pdb -o prot_processed.gro -water spce #Choisir OPLS (15)
+printf "15 0\n" | gmx pdb2gmx -f $1\_clean.pdb -o $1\_processed.gro -water spce #Choisir OPLS (15)
 
 # Step Three: Defining the Unit Cell & Adding Solvent
-gmx editconf -f prot_processed.gro -o prot_newbox.gro -c -d 1.0 -bt cubic # d diatance sur les bord; bt box type
-gmx solvate -cp prot_newbox.gro -cs spc216.gro -o prot_solv.gro -p topol.top
+gmx editconf -f $1\_processed.gro -o $1\_newbox.gro -c -d 1.0 -bt cubic # d diatance sur les bord; bt box type
+gmx solvate -cp $1\_newbox.gro -cs spc216.gro -o $1\_solv.gro -p topol.top
 # Step Four: Adding Ions (wget http://www.mdtutorials.com/gmx/lysozyme/Files/ions.mdp)  
-gmx grompp -f $configs/ions.mdp -c prot_solv.gro -p topol.top -o ions.tpr
-printf "13 0\n" | gmx genion -s ions.tpr -o prot_solv_ions.gro -p topol.top -pname NA -nname CL -neutral #choisir group 13 "SOL"
+gmx grompp -f ions.mdp -c $1\_solv.gro -p topol.top -o ions.tpr
+printf "13 0\n" | gmx genion -s ions.tpr -o $1\_solv_ions.gro -p topol.top -pname NA -nname CL -neutral #choisir group 13 "SOL"
 
 # Step Five: Energy Minimization (wget http://www.mdtutorials.com/gmx/lysozyme/Files/minim.mdp) 
-gmx grompp -f $configs/minim.mdp -c prot_solv_ions.gro -p topol.top -o em.tpr
+gmx grompp -f minim.mdp -c $1\_solv_ions.gro -p topol.top -o em.tpr
 gmx mdrun -v -deffnm em # attention
 printf "10 0\n" | gmx energy -f em.edr -o potential.xvg
 # At the prompt, type "10 0" to select Potential (10); zero (0) terminates input. 
 
 # Step Six: Equilibration (wget http://www.mdtutorials.com/gmx/lysozyme/Files/nvt.mdp) 
-gmx grompp -f $configs/nvt.mdp -c em.gro -r em.gro -p topol.top -o nvt.tpr
+gmx grompp -f nvt.mdp -c em.gro -r em.gro -p topol.top -o nvt.tpr
 gmx mdrun -deffnm nvt -v
 printf "16 0\n" | gmx energy -f nvt.edr -o temperature.xvg
 # Type "16 0" at the prompt to select the temperature of the system and exit
 
 # Step Seven: Equilibration 2 (wget http://www.mdtutorials.com/gmx/lysozyme/Files/npt.mdp) 
-gmx grompp -f $configs/npt.mdp -c nvt.gro -r nvt.gro -t nvt.cpt -p topol.top -o npt.tpr
+gmx grompp -f npt.mdp -c nvt.gro -r nvt.gro -t nvt.cpt -p topol.top -o npt.tpr
 gmx mdrun -deffnm npt -v
 printf "18 0\n" | gmx energy -f npt.edr -o pressure.xvg
 # Type "18 0" at the prompt to select the pressure of the system and exit. ATTENSION AU NUM
@@ -87,7 +70,7 @@ printf "24 0\n" | gmx energy -f npt.edr -o density.xvg
 # this time using energy and entering "24 0" at the prompt
 
 # Step Eight: Production MD (wget http://www.mdtutorials.com/gmx/lysozyme/Files/md.mdp)
-gmx grompp -f $configs/md.mdp -c npt.gro -t npt.cpt -p topol.top -o md_0_1.tpr
+gmx grompp -f md.mdp -c npt.gro -t npt.cpt -p topol.top -o md_0_1.tpr
 gmx mdrun -deffnm md_0_1 -nb gpu -v # utilise tous les gpu
 
 
